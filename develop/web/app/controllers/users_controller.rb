378bf13e21c857b138853e0b7b432ecac200f8f5
BadTudou <badtudou@gmail.com>
2017-03-29 11:49:40 +0800

    Add the controller of ‘users’

---
+++ develop/web/app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+	#the Web page of register
+	def new
+		render plain: 'get Web page of register'
+	end
+
+	#REST:create
+	def create
+	end
+
+	#REST:read
+	def show
+		#render plain: 'get Web page of show'
+	end
+
+	#the Web page of edit
+	def edit
+	end
+
+	#REST:update
+	def update
+	end
+
+	#REST:delete
+	def destroy
+	end
+end
