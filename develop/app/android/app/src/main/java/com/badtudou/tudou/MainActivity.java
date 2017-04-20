c930c13d20110e5aedf5ab3dae547f46f5f6e368
badtudou <badtudou@gmail.com>
2017-04-20 21:57:51 +0800

    Add sidebar by NavigationView

--- develop/app/android/app/src/main/java/com/badtudou/tudou/MainActivity.java
+++ develop/app/android/app/src/main/java/com/badtudou/tudou/MainActivity.java
@@ -69,7 +69,8 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_navigation);
 
         call = new Call(this);
         initFragments();
