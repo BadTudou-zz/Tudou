92adaf486f14b038ad9c88d84d9f6bad7ded383a
badtudou <badtudou@gmail.com>
2017-04-22 21:57:07 +0800

    Add details activity to display contact details info

--- develop/app/android/app/src/main/java/com/badtudou/tudou/ContactsListFragment.java
+++ develop/app/android/app/src/main/java/com/badtudou/tudou/ContactsListFragment.java
@@ -1,6 +1,7 @@
 package com.badtudou.tudou;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,6 +13,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -151,6 +153,15 @@ public class ContactsListFragment extends Fragment {
                 buttonClickListener.showMessage(R.id.button_switch_contact_style);
             }
         });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), DetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     /**
